329673a4f61ce1b47814349a683e53f6c766aab3
Geoffrey04 <geoffrey.do@hotmail.fr>
2025-09-09 09:50:50 +0200

    modif localhost

--- src/components/AttendanceTable.vue
+++ src/components/AttendanceTable.vue
@@ -53,7 +53,7 @@ const props = defineProps({
   },
 })
 
-const API = import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
+const API = import.meta.env.VITE_API_URL ?? 'https://emm-pointage.fr/'
 
 const headers = [
   { title: 'Élève', key: 'fullname', sortable: true },
